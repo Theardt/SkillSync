b5d0859a8918adc9fc39dc0179c771491884fc56
Nocturna198 <dominiquebarker01@gmail.com>
2026-05-29 22:09:06 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ void main() async {
   
  // CONTROLLED SEEDING (safe toggle)
 
-bool shouldSeedFirestore = true;
+bool shouldSeedFirestore = false;
 
   // ignore: dead_code
   if (shouldSeedFirestore) {
@@ -31,9 +31,9 @@ bool shouldSeedFirestore = true;
     } catch (e) {
       debugPrint("Seed error: $e");
     }
-  }// else {
-    //debugPrint("Firestore seeding skipped");
-  //}
+  }else {
+    debugPrint("Firestore seeding skipped");
+  }
 
   runApp(const SkillSyncApp());
 }
